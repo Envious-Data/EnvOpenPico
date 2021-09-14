58944340f18d5c75a4bf5386d9602e88498fb0e0
EnviousData <Enviousdata@gmail.com>
2021-09-14 15:04:17 +0100

    dont mind my indecision, generated updated gerbers and stuff.

--- EnvOpenPico/GenerateBOMCPL.bat
+++ EnvOpenPico/GenerateBOMCPL.bat
@@ -1,2 +1,2 @@
-jlc-kicad-tools -n EnvOpenPico . -o .\_output\ --include-all-component-groups
+jlc-kicad-tools -n EnvOpenPico . -o .\_output\ --include-all-component-groups --assume-same-lcsc-partnumber
 pause
