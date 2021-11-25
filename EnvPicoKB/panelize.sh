7a1d66acc78aa612a4c359371d8054a23c3784ac
EnviousData <Enviousdata@gmail.com>
2021-11-25 17:56:14 +0000

    Created Keyboard specific version that has a JST connector compatible with the unified daughterboard by ai03-2727

---
+++ EnvPicoKB/panelize.sh
@@ -0,0 +1,1 @@
+kikit panelize --layout 'grid; rows: 2; cols: 3; space: 2.5mm;' --tabs annotation --source 'tolerance: 15mm' --cuts 'mousebites; drill: 0.2mm; spacing: 0.4mm;' --framing 'railstb; width: 2.5mm; space: 2.5mm;' --post 'millradius: 1mm' EnvOpenPico.kicad_pcb ./_panel/EnvOpenPicoPanel.kicad_pcb
